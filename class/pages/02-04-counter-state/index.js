a9d1f56e4913d81842bda6618a0255d833f7b2b1
minseong <wsc7202@gmail.com>
2021-11-02 16:28:42 +0900

    2일차 완성

---
+++ class/pages/02-04-counter-state/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export default function CounterStatePage() {
+
+  const [qqq, setQqq] = useState(0);
+
+  function zzz() {
+    setQqq(qqq + 1)
+  }
+
+  return (
+    <>
+      <div> {qqq} </div>
+      <button onClick={zzz}>카운트증가</button>
+    </>
+  )
+}
