40f32df76e435e7286bd1a1e1aea5abeec6b4c12
miiin_sseong <wsc03002@naver.com>
2021-12-02 17:02:14 +0900

    23일차 추가

--- class/pages/23-05-login-success/index.tsx
+++ class/pages/23-05-login-success/index.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useApolloClient, useLazyQuery, useQuery } from "@apollo/client";
 import { IQuery } from "../../src/commons/types/generated/types";
 import { withAuth } from "../../src/components/commons/hocs/withAuth";
 
@@ -14,7 +14,14 @@ const FETCH_USER_LOGGED_IN = gql`
 
 function LoginSuccess() {
   const { data } =
-    useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN);
+    useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN); // 페이지 시작하자마자 요청 (모든게 자동)
+
+  // LazyQuery =요청시 기다린다.
+  // const [fetchUser, { data }] = useLazyQuery(FETCH_USER_LOGGED_IN); // 원하는곳에서 실행
+
+  // const client = useApolloClient();
+  // const result = client.query({}); // 원하는 곳에서 실행,원하는 변수에 담기
+
   return (
     <>
       <div>로그인 성공하였습니다.</div>
