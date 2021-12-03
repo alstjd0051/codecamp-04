5a851badc4326e21cd43679811889ae1d26db0db
miiin_sseong <wsc03002@naver.com>
2021-12-03 15:30:17 +0900

    구조분해할당

---
+++ 구조분해할당.js
@@ -0,0 +1,65 @@
+/* /*  const child = {
+  name: "영희",
+  age: 7,
+  school: "토끼초등학교"
+}
+
+
+const { name } = child();
+
+
+function getChild() {
+  return {
+    name: "철수",
+    age: 13,
+    school: "다람쥐초등학교"
+  }
+}
+
+const { getChild } = child2();
+
+console.log(school)
+
+
+function getClassmaates() {
+  child()
+
+  getChild()
+
+}
+
+const [child1, child2] = getClassmaates() */
+
+/* const child = {
+  name: "영희",
+  age: 7,
+  school: "토끼초등학교"
+}
+
+const { name } = child;
+
+console.log(name)
+
+
+function getChild() {
+  return {
+    name: "철수",
+    age: 13,
+    school: "다람쥐초등학교"
+  }
+}
+
+const { name } = child2;
+
+
+  */
+/* function getClassmaates(child1,child2) {
+
+  return(child2,child1)
+
+}
+
+const [child1, child2] = getClassmaates("훈이","맹구")
+
+console.log(child1)
+console.log(child2) */
